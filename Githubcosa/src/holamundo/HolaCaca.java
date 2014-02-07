e5b12dea7b76d8d3bb6a2fed944d902f22f32e7d
Sagüés <Sags@172.26.5.230>
2014-02-07 18:29:09 +0100

    commit 1 y 1 /4

---
+++ Githubcosa/src/holamundo/HolaCaca.java
@@ -0,0 +1,10 @@
+package holamundo;
+//Hecho por José Mª Sagüés
+public class HolaCaca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola caca");
+	}
+
+}
